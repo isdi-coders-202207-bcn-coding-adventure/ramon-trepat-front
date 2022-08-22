217182916476670232745e07807620ece8e76a9b
Ramon Trepat <r.trepis@gmail.com>
2022-08-22 15:24:22 +0200

    Changs component from app to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import Counter from "./components/Counter/Counter";
 import Layout from "./components/Layout/Layout";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Layout />
-      <Counter />
-    </>
-  );
+  return <Layout />;
 };
 
 export default App;
