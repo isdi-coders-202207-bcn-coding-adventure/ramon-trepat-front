a06632408bc3f7935d7d0eaac2cb7440ff000edc
Ramon Trepat <r.trepis@gmail.com>
2022-08-22 15:11:01 +0200

    Added component Counter in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Counter from "./components/Counter/Counter";
 import Layout from "./components/Layout/Layout";
 
 const App = (): JSX.Element => {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <Counter />
+    </>
+  );
 };
 
 export default App;
