69f4ce27ffbeeb1bd6bc296c90e9c6090f312434
Ramon Trepat <r.trepis@gmail.com>
2022-08-22 15:06:49 +0200

    Added component counter

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,26 @@
+const Counter = () => {
+  return (
+    <>
+      <div className="container">
+        <section className="section">
+          <span className="number">02</span>
+          <h2 className="text">days</h2>
+        </section>
+        <section className="section">
+          <span className="number">20</span>
+          <h2 className="text">hours</h2>
+        </section>
+        <section className="section">
+          <span className="number">34</span>
+          <h2 className="text">minutes</h2>
+        </section>
+        <section className="section">
+          <span className="number">09</span>
+          <h2 className="text">seconds</h2>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
