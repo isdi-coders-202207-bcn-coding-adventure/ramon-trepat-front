8ce9bc16334389c94ae6800f238922fe38f05da8
Ramon Trepat <r.trepis@gmail.com>
2022-08-22 15:22:26 +0200

    Changs Html and className

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,24 +1,24 @@
 const Counter = () => {
   return (
     <>
-      <div className="container">
-        <section className="section">
-          <span className="number">02</span>
-          <h2 className="text">days</h2>
-        </section>
-        <section className="section">
-          <span className="number">20</span>
-          <h2 className="text">hours</h2>
-        </section>
-        <section className="section">
-          <span className="number">34</span>
-          <h2 className="text">minutes</h2>
-        </section>
-        <section className="section">
-          <span className="number">09</span>
-          <h2 className="text">seconds</h2>
-        </section>
-      </div>
+      <ul className="numbers">
+        <li className="number">
+          <span className="number__digit">02</span>
+          <span className="number__title">days</span>
+        </li>
+        <li className="number">
+          <span className="number__digit">20</span>
+          <span className="number__title">hours</span>
+        </li>
+        <li className="number">
+          <span className="number__digit">34</span>
+          <span className="number__title">minutes</span>
+        </li>
+        <li className="number">
+          <span className="number__digit">09</span>
+          <span className="number__title">seconds</span>
+        </li>
+      </ul>
     </>
   );
 };
