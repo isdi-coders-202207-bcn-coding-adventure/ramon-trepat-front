1e7dbbfa3e7f2b8d04ee9c1f4144aa8ffba2f0a4
Ramon Trepat <r.trepis@gmail.com>
2022-08-25 12:55:28 +0200

    Added tested counter digit component

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,7 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../store/features/counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
 
 export type AppDispatch = typeof store.dispatch;
